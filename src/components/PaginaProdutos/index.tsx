96c02987ccfae02a52db5defe078ea8c0583be89
Augusto <augustovmagro@gmail.com>
2025-10-08 22:58:13 -0300

    Finishing product page

--- src/components/PaginaProdutos/index.tsx
+++ src/components/PaginaProdutos/index.tsx
@@ -7,7 +7,7 @@ const PaginaProdutos = () => {
 
 	return (
 		<>
-			<div className="p-10 bg-white dark:bg-gray-magros-700 text-gray-magros-700 dark:text-gray-magros-100">
+			<div className="p-10 min-h-[calc(100vh)] bg-white dark:bg-gray-magros-700 text-gray-magros-700 dark:text-gray-magros-100">
 				<div className="mt-10 sm:mx-0 md:mx-0 lg:mx-48 flex flex-wrap gap-5 justify-center">
 					{produtos?.map(item => (<ProdutoSector produto={item} />))}
 				</div>
--- src/components/ProdutoDetalhe/index.tsx
+++ src/components/ProdutoDetalhe/index.tsx
@@ -8,13 +8,25 @@ const ProdutoDetalhe = () => {
   const produto = produtos.filter(item => id == String(item.id))
 
   return (
-    <div className="p-10">
-      <div className="mt-10 flex bg-gray-magros-100 sm:mx-0 md:mx-0 lg:mx-48 gap-5 justify-center">
-        <img src={`/src/assets/${produto[0].img}`} alt="Breu para violino" className="w-200"/>
-        <div className="ms-5 flex flex-col">
-          {produto[0].descricao}
-          {produto[0].nome}
-          {produto[0].valor}
+    <div className="p-10 bg-white dark:bg-gray-magros-700 min-h-[calc(100vh)]">
+      <div className="mt-10 sm:mx-0 md:mx-0 lg:mx-48 gap-5 justify-center">
+        <div className="flex justify-center">
+          <div>
+            <img src={`/src/assets/${produto[0].img}`} alt="Breu para violino" className="w-200 object-fill rounded-s-md"/>
+          </div>
+          <div className="bg-gray-magros-100 dark:text-gray-magros-100 dark:bg-gray-magros-600 flex flex-col min-w-130 max-w-130 rounded-e-md">
+            <div className="p-10">
+              <h1 className=" font-bold text-3xl">
+                {produto[0].nome}
+              </h1>
+              <p className="mt-5">
+                {produto[0].descricao}
+              </p>
+              <h2 className="mt-10 text-2xl">
+                {produto[0].valor}
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
